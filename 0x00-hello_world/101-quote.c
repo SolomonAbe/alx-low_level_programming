ce62c8e9bde790a8bda2876feddfc3bdba014f99
SolomonAbe <soabebei@gmail.com>
2022-04-08 17:22:29 +0300

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error without printf and puts also needs to return 1 done via fputs function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
-
+i
 /**
  * main - Entry point
  *
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
