7c66c4bf16d88c664dcaafeec310d73e80dfdb16
SolomonAbe <soabebei@gmail.com>
2022-04-12 19:09:44 +0300

    adds two integers and returns the result

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,7 +4,7 @@
  * add - calcualte sum of two integers i and j then returns the result
  * @i: value one
  * @j: value two
- * Return - sum of i & j
+ * Return: sum of i & j
  */
 int add(int i, int j)
 {
