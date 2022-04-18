dcbb48296814435ab3de249602ab7eef3293dc15
SolomonAbe <soabebei@gmail.com>
2022-04-18 15:54:49 +0300

    function that takes a pointer to an int as parameter and updates the value it points to to 98

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values of two integers
+ * @a: value one
+ * @b: value two
+ * Return: null
+ */
+void swap_int(int *a, int *b)
+{
+
+}
