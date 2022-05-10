ab19c46727749107f186f6c6e55df8bc5042e994
SolomonAbe <soabebei@gmail.com>
2022-05-10 16:08:26 +0300

    define a new type struct

--- dog.h
+++ dog.h
@@ -1,23 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _DOG_H
+#define _DOG_H
+
 /**
- * struct dog - this descripction about which dog
- * @name: sdfasdf
- * @age:  asdfas
- * @owner:  asdf
+ *struct dog - dog description
+ *@name: it's name
+ *@age: it's age
+ *@owner: it's owner
+ *
  */
 
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
-typedef struct dog dog_t;
-dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int _strlen(char *s);
+
 #endif
