58c82d1fe0048e65152c2582011e745b08863d93
SolomonAbe <soabebei@gmail.com>
2022-05-20 18:44:30 +0300

    updating Task

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -15,8 +15,12 @@ size_t list_len(const list_t *h)
 
 	while (h != NULL)
 	{
-		count++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
+		count++;
 	}
 	return (count);
 }
